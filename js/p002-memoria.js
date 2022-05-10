40feea7d21c6f97e932505c844b12365d4df25de
PolRubio <apocrb23@gmail.com>
2022-05-10 19:10:44 +0200

    first commit

---
+++ js/p002-memoria.js
@@ -0,0 +1,50 @@
+var ampladaCarta, alcadaCarta;
+var separacioH = 20, separacioV = 20;
+var nFiles = 4, nColumnes = 4;
+
+var jocCartes = [
+    'carta1', 'carta1',
+    'carta39', 'carta39',
+]
+
+function generarDivs(){
+    var htmlcartas = '';
+    for (f = 1; f <= nFiles; f++){
+        for (c = 1; c <= nColumnes; c++) {
+            htmlcartas += '<div class="carta" id="f'+f+'c'+c+'"><div class="cara darrera"></div><div class="cara davant"></div></div>';
+        }
+        htmlcartas += '\n';
+    }
+    return htmlcartas;
+}
+
+
+$(function () {
+    var f, c, carta;
+
+    $("#tauler").html(generarDivs());
+
+    ampladaCarta = $(".carta").width();
+    alcadaCarta = $(".carta").height();
+    // mida del tauler
+    $("#tauler").css({
+        "width": ampladaCarta * nColumnes + separacioH * (nColumnes + 1) + "px",
+        "height": alcadaCarta * nFiles + separacioV * (nFiles + 1) + "px"
+    });
+    // inicialitzem totes les cartes: posició
+    for (f = 1; f <= nFiles; f++)
+        for (c = 1; c <= nColumnes; c++) {
+            carta = $("#f" + f + "c" + c);
+            carta.css({
+                "left": ((c - 1) * (ampladaCarta + separacioH) + separacioH) + "px",
+                "top": ((f - 1) * (alcadaCarta + separacioV) + separacioV) + "px"
+            });
+            carta.find(".davant").addClass(jocCartes.pop());
+
+        }
+
+    $(".carta").click(function () {
+        $(this).toggleClass("carta-girada");
+    });
+
+});
