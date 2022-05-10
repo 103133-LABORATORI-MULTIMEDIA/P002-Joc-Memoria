88db71eea8ed91dd3ec5d5463cc506a2aac59c07
PolRubio <aporcrb23@gmail.com>
2022-05-10 19:21:55 +0200

    minor changes

--- js/p002-memoria.js
+++ js/p002-memoria.js
@@ -1,17 +1,29 @@
 var ampladaCarta, alcadaCarta;
-var separacioH = 20, separacioV = 20;
-var nFiles = 4, nColumnes = 4;
+var separacioH = 20,
+    separacioV = 20;
+var nFiles = 4,
+    nColumnes = 4;
 
 var jocCartes = [
     'carta1', 'carta1',
     'carta3', 'carta3',
 ]
 
-function generarDivs(){
+function generarjocCartes() {
+    var jocCartes = [];
+    for (var i = 0; i < nFiles; i++) {
+        for (var j = 0; j < nColumnes; j++) {
+            jocCartes.push('carta' + (i * nColumnes + j + 1));
+        }
+    }
+    return jocCartes;
+}
+
+function generarDivs() {
     var htmlcartas = '';
-    for (f = 1; f <= nFiles; f++){
+    for (f = 1; f <= nFiles; f++) {
         for (c = 1; c <= nColumnes; c++) {
-            htmlcartas += '<div class="carta" id="f'+f+'c'+c+'"><div class="cara darrera"></div><div class="cara davant"></div></div>';
+            htmlcartas += '<div class="carta" id="f' + f + 'c' + c + '"><div class="cara darrera"></div><div class="cara davant"></div></div>';
         }
         htmlcartas += '\n';
     }
@@ -19,7 +31,7 @@ function generarDivs(){
 }
 
 
-$(function () {
+$(function() {
     var f, c, carta;
 
     $("#tauler").html(generarDivs());
@@ -43,7 +55,7 @@ $(function () {
 
         }
 
-    $(".carta").click(function () {
+    $(".carta").click(function() {
         $(this).toggleClass("carta-girada");
     });
 
